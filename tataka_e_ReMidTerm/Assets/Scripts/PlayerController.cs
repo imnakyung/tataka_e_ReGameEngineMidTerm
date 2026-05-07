9518a17cd120e9ed01640e413d6b8bdb4592156f
imnakyung <imnakyung1005@gmail.com>
2026-05-07 15:52:11 +0900

    [Feat] 미완

--- tataka_e_ReMidTerm/Assets/Scripts/PlayerController.cs
+++ tataka_e_ReMidTerm/Assets/Scripts/PlayerController.cs
@@ -70,6 +70,9 @@ public class PlayerController : MonoBehaviour
 
         if (collision.CompareTag("Finish"))
         {
+            //HighScore.TrySet(SceneManager.GetActiveScene().buildIndex, (int)score);
+            StageResultSaver.SaveStage(SceneManager.GetActiveScene().buildIndex, (int)score);
+
             collision.GetComponent<LevelObject>().MoveToNextLevel();
         }
 
